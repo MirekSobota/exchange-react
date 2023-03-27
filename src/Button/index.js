ced80c381324ea9e7d0255fc73b631f32b304905
MirekSobota <miroslaw.sobota@gmail.com>
2023-03-27 12:40:52 +0200

    Moving input and buttonComponent to form component

--- src/Button/index.js
+++
@@ -1,9 +0,0 @@
-import "./style.css";
-
-const Button = () => (
-  <p>
-    <button className="button">Exchange</button>
-  </p>
-);
-
-export { Button };
--- src/Form/index.js
+++ src/Form/index.js
@@ -1,16 +1,33 @@
-const Form = ({ label, button }) => {
+import { useState } from "react";
+import "./style.css";
+
+const Form = ({ getNewInputValue }) => {
+  const [newInputValue, setNewInputValue] = useState("");
+  const onInputChange = ({ target }) => setNewInputValue(target.value);
   const onFormSubmit = (event) => {
     event.preventDefault();
+    getNewInputValue(newInputValue.trim());
+    setNewInputValue("");
   };
 
   return (
-    <>
-      <form onSubmit={onFormSubmit}>
-        {label}
-        {button}
-      </form>
-    </>
+    <form onSubmit={onFormSubmit} className="form ">
+      <input
+        value={newInputValue}
+        className="form__input"
+        required
+        type="number"
+        inputMode="numeric"
+        min="1"
+        step="any"
+        placeholder="Amount PLN:"
+        onChange={onInputChange}
+      />
+
+      <p>
+        <button className="form__button">Exchange</button>
+      </p>
+    </form>
   );
 };
-
 export { Form };
