2e11166ac072c051ee5a3a508761f8b67178a8ca
MirekSobota <miroslaw.sobota@gmail.com>
2023-04-08 14:25:08 +0200

    Create currencies.js and moving currencies array

--- src/Currencies/currencies.js
+++ src/Currencies/currencies.js
@@ -1,12 +1,8 @@
 
-const Currencies = () => {
-    const currencies = [
-        { id: 1, name: "EURO", rate: 4.55 },
-        { id: 2, name: "USD", rate: 3.95 },
-        { id: 3, name: "GBP", rate: 4.85 },
-      ]
-};
-    
+ export const currencies = [
+    { id: 1, name: "EURO", rate: 4.55 },
+    { id: 2, name: "USD", rate: 3.95 },
+    { id: 3, name: "GBP", rate: 4.85 },
+  ];
 
 
-  export {Currencies}
