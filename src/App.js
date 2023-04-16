80ca9fed02a88accbbd6032ac0816805ec8c65cb
MirekSobota <miroslaw.sobota@gmail.com>
2023-04-16 15:45:25 +0200

    Adding font and finish date and time

--- src/App.js
+++ src/App.js
@@ -14,20 +14,20 @@ function App() {
   const [inputValue, setInputValue] = useState("");
   const [selectCurrency, setSelectCurrency] = useState("EURO");
   const [result, setResult] = useState("");
-  const [date, setDate] = useState(new Date())
+  const [date, setDate] = useState(new Date());
 
-  useEffect  (() => {
+  useEffect(() => {
     const intervalId = setInterval(() => {
-      setDate(new Date())
+      setDate(new Date());
     }, 1000);
     return () => clearInterval(intervalId);
   }, []);
 
   const onChange =
     (setter) =>
-      ({ target }) => {
-        setter(target.value);
-      };
+    ({ target }) => {
+      setter(target.value);
+    };
 
   const onFormSubmit = (event) => {
     event.preventDefault();
@@ -50,17 +50,17 @@ function App() {
   const currencyInput = currencyRate ? currencyRate.rate : 0;
 
   const calculateResult = (inputValue, currencyInput) =>
-    `${inputValue +
-    " PLN  = " +
-    (inputValue / currencyInput).toFixed(2) +
-    selectCurrency
+    `${
+      inputValue +
+      " PLN  = " +
+      (inputValue / currencyInput).toFixed(2) +
+      selectCurrency
     }`;
 
   return (
     <Container>
       <SubContainer>
-        <Header title="Currency converter"
-          date={date} />
+        <Header title="Currency converter" date={date} />
         <SubHeader title="The current exchange rate" />
         <List currencies={currencies} />
         <Form
--- src/Header/index.js
+++ src/Header/index.js
@@ -3,9 +3,13 @@ import "./style.css";
 const Header = ({ title, date }) => (
   <header>
     <>
-    <p>{date.toLocaleDateString(undefined, { day: "long", day: "numeric", month: "long"})}</p>
-    <h1 className="header">{title}</h1>
-    </>    
+      <p className="header__date">
+        Today is {date.toLocaleDateString("en-EN", { weekday: "long" })},{" "}
+        {date.toLocaleDateString(undefined, { day: "numeric", month: "long" })},{" "}
+        {date.toLocaleTimeString()}
+      </p>
+      <h1 className="header">{title}</h1>
+    </>
   </header>
 );
 
