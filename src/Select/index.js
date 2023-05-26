1b5208366af200904373789b77bae7b25a8086db
MirekSobota <miroslaw.sobota@gmail.com>
2023-05-26 10:28:26 +0200

    Added styled-components to Select

--- src/Select/index.js
+++ src/Select/index.js
@@ -1,14 +1,14 @@
-import "./style.css";
+import { Content } from "./styled";
 
 const Select = ({ selectCurrency, onChange, currencies }) => {
   return (
-    <select className="select" value={selectCurrency} onChange={onChange}>
+    <Content value={selectCurrency} onChange={onChange}>
       {currencies.map((currency) => (
         <option key={currency.id} value={currency.name}>
           {currency.name}
         </option>
       ))}
-    </select>
+    </Content>
   );
 };
 
