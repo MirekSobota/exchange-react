72eece8eee7978f22d7c19fceede564df7a543b3
MirekSobota <miroslaw.sobota@gmail.com>
2023-10-05 16:21:18 +0200

    Changed API url

--- src/App/useApiData.js
+++ src/App/useApiData.js
@@ -1,25 +1,37 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const requestURL =
-  "https://api.exchangerate.host/latest?base=EURO&symbols=USD,EUR,CZK,PLN,";
+const apiKey= "zCYVeigwdsIU19nZF1qgx2zjOpLNzb5K";
 
+const requestURL =  "https://api.apilayer.com/currency_data/live?source=EUR&currencies=USD,GBP,PLN";
+  
 export const useApiData = () => {
   const [apiData, setApiData] = useState({
     state: "loading",
   });
 
+
   useEffect(() => {
     const fetchRates = async () => {
       try {
-        const response = await axios.get(requestURL);
-        const { rates, date } = response.data;
-
+        const response = await axios.get(requestURL, {
+          headers: { apiKey }
+        });
+      
+        const { quotes, timestamp, success } = response.data;
+        console.log(response.data)
+        
+        if (success) {
+          const modifiedQuotes = Object.fromEntries(
+            Object.keys(quotes).map(key =>[key.substring(3), quotes[key]])
+          )
+    
         setApiData({
           state: "success",
-          rates,
-          date,
-        });
+          rates: modifiedQuotes,
+          date: new Date(timestamp * 1000).toLocaleDateString(),
+          
+        })};
       } catch {
         setApiData({
           state: "error",
